285812cf185195380f5199a487c969236c5fff0a
hashemt <hashemta77@gmail.com>
2019-08-28 10:33:02 +0300

    create server validation for login form Relates #7

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,14 +4,14 @@ const { getSignup } = require('./getSignup');
 const { postSignup } = require('./postSignup');
 const { getHome } = require('./getHome');
 const { getLogin } = require('./getLogin');
+const { postLogin } = require('./postLogin');
 
 const router = express.Router();
 
 router.get('/', getHome);
-
 router.get('/login', getLogin);
-
+router.post('/login', postLogin);
 router.get('/signup', getSignup);
-
 router.post('/signup', postSignup);
+
 module.exports = router;
