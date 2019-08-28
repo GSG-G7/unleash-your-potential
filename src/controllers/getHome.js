2e74fcf5e05010c0ce68510d7f194635ec829a92
rana <ranasobeid95@gmail.com>
2019-08-28 14:51:55 +0300

    solve conflict

--- src/controllers/getHome.js
+++ src/controllers/getHome.js
@@ -3,8 +3,6 @@ const { getPosts } = require('../database/queries/getPost');
 exports.getHome = (req, res, next) => {
   getPosts()
     .then((posts) => {
-      console.log(posts.rows);
-
       res.render('home', { allPosts: posts.rows });
     })
     .catch((err) => next(err.stacks));
