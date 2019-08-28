a64bbb0f8f2ec0bd49cde665eace9a6749a92e1d
hashemt <hashemta77@gmail.com>
2019-08-28 16:54:52 +0300

    handle profile

--- src/database/queries/getUserById.js
+++ src/database/queries/getUserById.js
@@ -1,7 +1,7 @@
 const dbconnection = require('../config/connection');
 
 exports.getUserById = (data) => {
-  const { id } = data;
+  const  id  = data;
   return dbconnection.query({
     text: 'SELECT * FROM users WHERE id = $1',
     values: [id],
