8cd8d1e39458a0e5f6d64963fced65425c53f5fd
hashemt <hashemta77@gmail.com>
2019-08-29 10:15:47 +0300

    solve issues

--- src/controllers/addPost.js
+++ src/controllers/addPost.js
@@ -1,7 +1,7 @@
 const { addPost } = require('../database/queries/addPost');
 
 exports.addPost = (req, res, next) => {
-  addPost(req.body)
-    .then((result) => res.redirect('/'))
+  addPost(req.body, req.cookie.id)
+    .then(() => res.redirect('/'))
     .catch((err) => next(err.stack));
 };
--- src/database/queries/addPost.js
+++ src/database/queries/addPost.js
@@ -2,8 +2,8 @@ const dbconnection = require('../config/connection');
 
 exports.addPost = (data, userId) => {
   const { title, description, content } = data;
-  const d = new Date();
-  const publishedAt = `${d.getDate()} / ${d.getMonth() + 1} / ${d.getFullYear()}`;
+  const date = new Date();
+  const publishedAt = `${date.getDate()} / ${date.getMonth() + 1} / ${date.getFullYear()}`;
   return dbconnection.query({
     text: 'INSERT INTO posts (title, description, content, published_at, user_id) VALUES ($1, $2, $3, $4, $5);',
     values: [title, description, content, publishedAt, userId],
