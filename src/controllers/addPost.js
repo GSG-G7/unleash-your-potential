79f0287e701b27c4db1d7be832871c1b76de1144
hashemt <hashemta77@gmail.com>
2019-08-29 10:46:10 +0300

    solve issues Relates #38 #25

--- src/controllers/addPost.js
+++ src/controllers/addPost.js
@@ -1,7 +1,7 @@
 const { addPost } = require('../database/queries/addPost');
 
 exports.addPost = (req, res, next) => {
-  addPost(req.body, req.cookie.id)
+  addPost(req.body, req.cookies.id)
     .then(() => res.redirect('/'))
     .catch((err) => next(err.stack));
 };
